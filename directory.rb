281e14a877358d89c83dccc0c0b49fae5dd4bbdf
Evan Thomas <evanthomas0292@gmail.com>
2023-04-03 10:25:13 +0100

    Add interactive menu.

--- directory.rb
+++ directory.rb
@@ -171,7 +171,34 @@ def main_with_interactivity
 end
 
 def main
-  print_header
-  print(STUDENTS)
-  print_footer(STUDENTS)
+  interative_menu
 end
+
+def interative_menu
+  students = []
+  loop do
+    # print the menu and ask the user what to do
+    puts '1. Input the students'
+    puts '2. Show the students'
+    puts '9. Exit'
+    # read the input and save it into a variable
+    selection = gets.chomp
+    # do what the use has asked
+    case selection
+    when '1'
+      # input the students
+      students = input_students
+    when '2'
+      # show the students
+      print_header
+      print(students)
+      print_footer(students)
+    when '9'
+      exit
+    else
+      puts "I don't know what you meant. Try again."
+    end
+  end
+end
+
+main
